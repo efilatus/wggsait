94867e6f0eb000fd6433a9a505f9cf5ea5df6e23
WGG <wgg@MacBook-Air-WGG.local>
2018-10-25 17:56:38 +0300

    начало работы

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    
+//    $(window).scroll(function() {
+//        if ($(this).scrollTop() > 1){
+//            $('h1').addClass("sticky");
+//        }
+//        else{
+//            $('h1').removeClass("sticky");
+//        }
+//    });
+    
+    
+    
+    
+});
