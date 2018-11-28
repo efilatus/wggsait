a6f70a60d337cfffb953d60a837aee9231f65204
Eduard <efilatus@mail.ru>
2018-11-28 15:30:52 +0300

    last commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 $(document).ready(function () {
     
-//    $(window).scroll(function() {
-//        if ($(this).scrollTop() > 1){
-//            $('h1').addClass("sticky");
-//        }
-//        else{
-//            $('h1').removeClass("sticky");
-//        }
-//    });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 104){
+            $('#shapka').addClass("sticky");
+            $('#shfoo').css("padding-top", "54px");
+        }
+        else{
+            $('#shapka').removeClass("sticky");
+            $('#shfoo').css("padding-top", "0px");
+        }
+    });
     
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
         nav: true,
+        dots: false,
         items:1
     });
     $( ".owl-prev").html('<i class="fa fa-chevron-left"></i>');
